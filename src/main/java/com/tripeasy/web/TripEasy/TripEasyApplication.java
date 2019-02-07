b0df6f241db14302328fc06edb3e4463a693fdb6
Raut <=>
2019-02-07 16:09:28 +0530

    First Commit

---
+++ src/main/java/com/tripeasy/web/TripEasy/TripEasyApplication.java
@@ -0,0 +1,14 @@
+package com.tripeasy.web.TripEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripEasyApplication.class, args);
+	}
+
+}
+
