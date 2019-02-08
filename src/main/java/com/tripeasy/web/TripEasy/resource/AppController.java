77ad3ec821451268afade0c007064a318e20f860
tejeskhandagale400 <45350219+tejeskhandagale400@users.noreply.github.com>
2019-02-08 13:13:26 +0530

    T

--- src/main/java/com/tripeasy/web/TripEasy/resource/AppController.java
+++ src/main/java/com/tripeasy/web/TripEasy/resource/AppController.java
@@ -5,12 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-=======
->>>>>>> 789c8e8bb4becf8755c1cf2f4b3bf3916634d10e
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
