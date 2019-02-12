ab898b6a0cabe52f4d232368d1c4fe27405b868d
kanaksoni714 <kanaksoni714@gmail.com>
2019-02-12 18:03:30 +0530

    g

---
+++ src/main/java/com/tripeasy/web/TripEasy/resource/WalletController.java
@@ -0,0 +1,13 @@
+package com.tripeasy.web.TripEasy.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/wallet")
+public class WalletController {
+	/*
+	 * @Autowired WalletService service;
+	 */
+}
