aae346c34cb6663e68c95b9d04061469befd86e9
tejeskhandagale400 <45350219+tejeskhandagale400@users.noreply.github.com>
2019-02-13 16:15:35 +0530

    updates Hotel Controller

---
+++ src/main/java/com/tripeasy/web/TripEasy/pojo/HotelAddress.java
@@ -0,0 +1,169 @@
+package com.tripeasy.web.TripEasy.pojo;
+
+public class HotelAddress {
+	private String area;
+	private String landMark;
+	private String state;
+	private String nation;
+	private String city;
+	private Integer pincode;
+	private String lattitude;
+	private String longitude;
+
+	public HotelAddress() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HotelAddress(String area, String landMark, String state, String nation, String city, Integer pincode,
+			String lattitude, String longitude) {
+		super();
+		this.area = area;
+		this.landMark = landMark;
+		this.state = state;
+		this.nation = nation;
+		this.city = city;
+		this.pincode = pincode;
+		this.lattitude = lattitude;
+		this.longitude = longitude;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((area == null) ? 0 : area.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((landMark == null) ? 0 : landMark.hashCode());
+		result = prime * result + ((lattitude == null) ? 0 : lattitude.hashCode());
+		result = prime * result + ((longitude == null) ? 0 : longitude.hashCode());
+		result = prime * result + ((nation == null) ? 0 : nation.hashCode());
+		result = prime * result + ((pincode == null) ? 0 : pincode.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HotelAddress other = (HotelAddress) obj;
+		if (area == null) {
+			if (other.area != null)
+				return false;
+		} else if (!area.equals(other.area))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (landMark == null) {
+			if (other.landMark != null)
+				return false;
+		} else if (!landMark.equals(other.landMark))
+			return false;
+		if (lattitude == null) {
+			if (other.lattitude != null)
+				return false;
+		} else if (!lattitude.equals(other.lattitude))
+			return false;
+		if (longitude == null) {
+			if (other.longitude != null)
+				return false;
+		} else if (!longitude.equals(other.longitude))
+			return false;
+		if (nation == null) {
+			if (other.nation != null)
+				return false;
+		} else if (!nation.equals(other.nation))
+			return false;
+		if (pincode == null) {
+			if (other.pincode != null)
+				return false;
+		} else if (!pincode.equals(other.pincode))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		return true;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getLandMark() {
+		return landMark;
+	}
+
+	public void setLandMark(String landMark) {
+		this.landMark = landMark;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getNation() {
+		return nation;
+	}
+
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Integer getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(Integer pincode) {
+		this.pincode = pincode;
+	}
+
+	public String getLattitude() {
+		return lattitude;
+	}
+
+	public void setLattitude(String lattitude) {
+		this.lattitude = lattitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	@Override
+	public String toString() {
+		return "HotelAddress [area=" + area + ", landMark=" + landMark + ", state=" + state + ", nation=" + nation
+				+ ", city=" + city + ", pincode=" + pincode + ", lattitude=" + lattitude + ", longitude=" + longitude
+				+ "]";
+	}
+
+}
