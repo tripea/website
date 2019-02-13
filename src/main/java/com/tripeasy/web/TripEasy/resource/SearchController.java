e7495102762c0beb2e7cc2bcee3e8c8c93b23b52
tejeskhandagale400 <45350219+tejeskhandagale400@users.noreply.github.com>
2019-02-13 14:35:07 +0530

    Uph

--- src/main/java/com/tripeasy/web/TripEasy/resource/SearchController.java
+++ src/main/java/com/tripeasy/web/TripEasy/resource/SearchController.java
@@ -26,31 +26,22 @@ public class SearchController {
 
 	@RequestMapping("/searchHotels")
 	public ModelAndView getAllCityForHotel() {
-<<<<<<< HEAD
  		List<Hotel> hotelCityList = restTemplate.getForObject("http://10.246.92.61:8989/cities/Hotel", List.class);
  		return new ModelAndView("HotelSearch", "hotelCityList", hotelCityList);
  		}
-=======
-		List<Hotel> hotelList = restTemplate.getForObject("http://10.246.92.61:8989/cities/Hotel", List.class);
-		return new ModelAndView("HotelSearch", "hotelList", hotelList);
 
-	}
->>>>>>> 386bb27d51a888c90027c24f443077a6700bce49
 
 	@RequestMapping(value = "/searchFlight", method = RequestMethod.GET)
 	public String searchFlight(@RequestParam String source, @RequestParam String destination) {
 		// TODO change localhost to ip 10.246.92.123
 		return "redirect:http://localhost:8764/flight/flights?source=" + source + "&destination=" + destination;
 	}
-<<<<<<< HEAD
+	
+	
+	
+	
 	@RequestMapping(value= "/searchHotel", method=RequestMethod.GET)
 	public String searchHotel(@RequestParam String city) {
 		return "redirect:http://10.246.92.124:8764/hotel/hotels?city="+city;
-=======
-
-	@RequestMapping(value = "/searchHotel", method = RequestMethod.POST)
-	public String searchHotel(@RequestParam String city) {
-		return "redirect:http://localhost:9095/hotels/?city=" + city;
->>>>>>> 386bb27d51a888c90027c24f443077a6700bce49
 	}
 }
